8e7e1216b2545c34b248fdbec2b524d80680fac2
icampos <isaac.camposcruz@gmail.com>
2018-05-14 22:33:30 -0600

    Let Keyword

--- index.js
+++ index.js
@@ -1,3 +1,31 @@
-let greeting = "Hi";
+/*
 
-console.log(greeting);
+LET Exercise
+
+ - Let is similar to var, but only accesible to the block lever it is defined.
+ - Let variables are not hosited.
+
+*/ 
+
+let a = 50;
+let b = 100;
+if (true) {
+ let a = 60;
+ var c = 10;
+ console.log(a/c); // 6
+ console.log(b/c); // 10
+}
+console.log(c); // 10
+console.log(a); // 50
+
+
+let a = 50;
+let b = 100;
+if (true) {
+ let a = 60;
+ let c = 10;
+ console.log(a/c); // 6
+ console.log(b/c); // 10
+}
+console.log(c); // Undefined
+console.log(a); // 50
